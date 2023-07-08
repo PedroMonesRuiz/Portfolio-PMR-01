a518bf62648bc9dfe52f2cd47c4c624a6ff1fc76
PedroMonesRuiz <138902489+PedroMonesRuiz@users.noreply.github.com>
2023-07-07 21:18:28 -0300

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,48 @@
+<?php
+include("Mailer/src/PHPMailer.php");
+include("Mailer/src/Exception.php");
+include("Mailer/src/SMTP.php");
+
+try {
+    $fromname = $_POST['nombre'];
+    $fromemail = $_POST['email'];
+    $subject = "Correro dede tú Portfolio Web";
+    $mensa = $_POST['mensaje'];
+    $host = "smtp.gmail.com";
+    $port = "587";
+    $SMTPAuth = "login";
+    $SMTPSecure = "tls";
+    $usernamedestinatario = "monesruizpedro94@gmail.com";
+    $username = "pmr.pruebas.programacion@gmail.com";
+    $password = "vcvhoyxcjpdxfpyy";
+
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    $mail->isSMTP();
+    $mail->SMTPDebug = 0;
+    $mail->Host = $host;
+    $mail->Port = $port;
+    $mail->SMTPAuth = $SMTPAuth;
+    $mail->SMTPSecure = $SMTPSecure;
+    $mail->Username = $username;
+    $mail->Password = $password;
+    $mail->setFrom($fromemail,$fromname);
+    //DESTINATARIO
+    $mail->addAddress($usernamedestinatario);
+    //ASUSNTO
+    $mail->isHTML(true);
+    $mail->Subject = $subject;
+    //CUERPO DEL EMAIL
+    $mensaje = nl2br("Este mensaje fue enviado por " . $fromname . " \n");
+    $mensaje .= nl2br("Su e-mail es: " . $fromemail . " \n");
+    $mensaje .= nl2br("Mensaje: " . $mensa . " \n");
+    $mensaje .= "Enviado el " . date('d/m/Y', time());
+    $mail->Body = $mensaje;
+
+    if (!$mail->send()) {
+        error_log("MAILER: no se pudo enviar el correo!");
+        echo("MAILER: no se pudo enviar el correo!");
+    }
+} catch (Exception $e) {
+    echo("Alguna instrucción de PHPMailer no se pudo llevar a cabo " + $e);
+}
+?>
